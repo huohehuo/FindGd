f4ff86ff6c7d5777450cfd27f89fcdd98dbad190
cyber <753392431@qq.com>
2017-01-05 09:57:06 +0800

    左侧圆形头像OK，百度地图SDK集成OK

---
+++ app/src/main/java/com/feicui/findgd/TreasureApplication.java
@@ -0,0 +1,21 @@
+package com.feicui.findgd;
+
+import android.app.Application;
+
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * Created by LINS on 2017/1/4.
+ * Please Try Hard
+ */
+public class TreasureApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+
+
+    }
+}
--- app/src/main/java/com/feicui/findgd/treasure/HomeActivity.java
+++ app/src/main/java/com/feicui/findgd/treasure/HomeActivity.java
@@ -2,18 +2,83 @@ package com.feicui.findgd.treasure;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.feicui.findgd.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by LINS on 2017/1/3.
  * Please Try Hard
  */
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.navigation)
+    NavigationView mNavigationView;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        // toolbar的处理
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        // drawerLayout设置监听
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        toggle.syncState();// 同步状态
+        mDrawerLayout.addDrawerListener(toggle);
+        // 设置Navigation每一条的选中监听
+        mNavigationView.setNavigationItemSelectedListener(this);
+        ImageView ivIcon = (ImageView) mNavigationView.getHeaderView(0).findViewById(R.id.iv_usericon);
+        ivIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 更换头像
+            }
+        });
+    }
+    // Navigation每一条的选中监听
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_hide:
+                break;
+            case R.id.menu_logout:
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    // 处理back返回键
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
